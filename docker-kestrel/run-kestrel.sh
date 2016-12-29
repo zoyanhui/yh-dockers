d9f029ae2de210717a8b1d266ec1fbf34afa76db
zhouyanhui@kanzhun.com <zhouyanhui@kanzhun.com>
2016-12-29 13:31:07 +0800

    init commit

---
+++ docker-kestrel/run-kestrel.sh
@@ -0,0 +1,1 @@
+docker run -p 22133:22133 -v /data/kestrel:/data -d zoyanhui/kestrel:latest --name kestrel-container-name
---
+++ docker-mongo/run-mongo.sh
@@ -0,0 +1,1 @@
+docker run --name mongo-container-name -v /data/mongodb:/data/db -p 27017:27017 -d mongo --noprealloc --smallfiles
